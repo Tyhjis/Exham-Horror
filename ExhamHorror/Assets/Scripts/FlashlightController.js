afb3d77d0119dabbb42c1598e4fd935012ffbc3a
VT <valtter.taipale@gmail.com>
2013-10-23 17:30:33 +0300

    projekti luotu, Standard Assetteja lisätty hieman ja omia Scriptejä.

---
+++ ExhamHorror/Assets/Scripts/FlashlightController.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+
+class FlashlightController extends MonoBehaviour{
+	 
+	public var flashlight : Light;
+	
+	private var NextShot = 0.0f;
+	public var ChargeTime = 0.5f;
+	
+	function Start () {
+		
+	}
+	
+	function Update() {
+
+		if (Input.GetButton("Fire2")){
+			 StopCoroutine("LightChange");
+	         StartCoroutine("LightChange");
+		}
+	
+	}
+	
+	function LightChange(){
+	
+		if (Time.time < NextShot){
+		 	yield;
+		}
+		NextShot = Time.time + ChargeTime;
+		
+
+		if(flashlight.enabled == true){		
+			flashlight.enabled = false;		
+		}
+		else if(flashlight.enabled == false){		
+			flashlight.enabled = true;			
+		}
+
+
+
+		yield;
+	
+	}
+
+}
---
+++ ExhamHorror/Assets/Scripts/ShootingController.js
@@ -0,0 +1,43 @@
+﻿#pragma strict
+
+class ShootingController extends MonoBehaviour{
+	 
+	public var projectile : Rigidbody;
+	public var shooter : Transform;
+	public var projectileSpeed = 80;
+	
+	
+	private var NextShot = 0.0f;
+	public var ChargeTime = 0.5f;
+	
+	function Start () {
+		
+	}
+	
+	function Update() {
+
+		if (Input.GetButton("Fire1")){
+		
+			if (Time.time > NextShot){
+				 StopCoroutine("Shoot");
+		         StartCoroutine("Shoot");
+		         NextShot = Time.time + ChargeTime;
+	        }
+		}
+	
+	}
+	
+	function Shoot(){
+	
+		var bullet : Rigidbody;
+		bullet = Instantiate (projectile, transform.position , transform.rotation); 
+	
+		//var directionVector = new Vector3(shooter.transform.forward);
+		
+		//bullet.velocity = transform.TransformDirection (shooter.transform.forward * projectileSpeed);
+		bullet.rigidbody.AddForce(shooter.transform.forward * projectileSpeed);
+		yield;
+	
+	}
+
+}
